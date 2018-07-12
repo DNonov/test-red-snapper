a5166e03a0078a97cfe68d63c2ed23a1fd168ea8
Dimitar Nonov <d.nonov@gmail.com>
2018-07-12 19:57:30 +0100

    Add extra back button
    Add back button with small css animation. Plus extra bits for the
    mobile version.

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -5,9 +5,15 @@ import "./HeroImage.css";
 class HeroImage extends Component {
 	render() {
 		return (
-			<div className="hero-image-section">
-				<img src={SteveJobsImage} alt="steve-jobs-presenting"></img>
-			</div>
+			<React.Fragment>
+				<div className="arrow-back">
+					<i className="zmdi zmdi-chevron-left"></i>
+					<span className="arrow-back-text">Back</span>
+				</div>
+				<div className="hero-image-section">
+					<img src={SteveJobsImage} alt="steve-jobs-presenting"></img>
+				</div>
+			</React.Fragment>
 		)
 	}
 }
