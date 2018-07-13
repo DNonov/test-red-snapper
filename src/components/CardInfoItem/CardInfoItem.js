bb626259b3b17e9b19ff6173f9af2a8d0da62c94
Dimitar Nonov <d.nonov@gmail.com>
2018-07-13 13:23:19 +0100

    Refacor CardInfoItem component

--- src/components/CardInfoItem/CardInfoItem.js
+++ src/components/CardInfoItem/CardInfoItem.js
@@ -5,8 +5,8 @@ class CardInfoItem extends Component {
 	render() {
 		return (
 			<div className="card-info-item">
-				<span className="card-info-item quantity">{this.props.quantity}</span>
-				<span className="card-info-item subject">{this.props.subject}</span>
+				<span className="card-info-item-quantity">{this.props.quantity}</span>
+				<span className="card-info-item-subject">{this.props.subject}</span>
 			</div>
 		)
 	}
